430a9418e8a6a0238f2cf83b8082157da41714db
Gabrielle Ribeiro <33001620+Gabrielle-Ribeiro@users.noreply.github.com>
2024-03-11 22:07:22 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+=======
 import './index.css';
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+>>>>>>> 4bd3f83ad926b0dbc195ab09bf43e40ac47a64d5
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
