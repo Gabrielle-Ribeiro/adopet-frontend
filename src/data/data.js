5a29d17e3593c8c7034da795265a568d8d7480d1
Gabrielle Ribeiro <33001620+Gabrielle-Ribeiro@users.noreply.github.com>
2024-03-13 20:42:15 -0300

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,5 @@
 export const pets = [
+  <<<<<<< HEAD
   { img: 'Dunga.png', name: 'Dunga', age: '2 anos', size: 'Porte pequeno', behavior: 'Calmo e educado', city: 'Rio de Janeiro (RJ)' },
   { img: 'Felicia.png', name: 'Felícia', age: '3 meses', size: 'Porte pequeno', behavior: 'Ativa e carinhosa', city: 'Nova Iguaçu (RJ)' },
   { img: 'Sirius.png', name: 'Sirius', age: '6 meses', size: 'Porte grande', behavior: 'Ativa e educado', city: 'Duque de Caxias (RJ)' },
@@ -8,4 +9,11 @@ export const pets = [
   { img: 'Lua.png', name: 'Lua', age: '6 meses', size: 'Porte médio', behavior: 'Ativo e carinhosa', city: 'Duque de Caxias (RJ)' },
   { img: 'Amora.png', name: 'Amora', age: '45 dias', size: 'Porte grande', behavior: 'Calma e carinhosa', city: 'São Gonçalo (RJ)' },
   { img: 'Zelda.png', name: 'Zelda', age: '5 meses', size: 'Porte médio', behavior: 'Ativa e amável', city: 'Rio de Janeiro (RJ)' },
+  =======
+  { img: 'Fiona.png', name: 'Fiona', age: '3 anos', size: 'Porte pequeno', behavior: 'Calma e carinhosa', city: 'São Gonçalo (RJ)' },
+  { img: 'Yoda.png', name: 'Yoda', age: '1 ano', size: 'Porte médio', behavior: 'Ativo e carinhoso', city: 'Nova Iguaçu (RJ)' },
+  { img: 'Dunga.png', name: 'Dunga', age: '2 anos', size: 'Porte pequeno', behavior: 'Calmo e educado', city: 'Rio de Janeiro (RJ)' },
+  { img: 'Sid.png', name: 'Sid', age: '8 meses', size: 'Porte médio/grande', behavior: 'Brincalhão e amável', city: 'Rio de Janeiro (RJ)' },
+  { img: 'Zelda.png', name: 'Zelda', age: '5 meses', size: 'Porte médio', behavior: 'Ativa e amável', city: 'Rio de Janeiro (RJ)' },
+  >>>>>>> 4bd3f83ad926b0dbc195ab09bf43e40ac47a64d5
 ];
