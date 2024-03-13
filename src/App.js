67bf4c8b0a8a4a5c180fb9af5b5544217c5d6088
Gabrielle Ribeiro <33001620+Gabrielle-Ribeiro@users.noreply.github.com>
2024-03-13 20:39:18 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+<<<<<<< HEAD
 // dependencies
 import { BrowserRouter as Router } from "react-router-dom";
 // components
 import AnimatedRoutes from "./Components/AnimatedRoutes.js";
-
+=======
+// components
+import AnimatedRoutes from "./Components/AnimatedRoutes.js";
+// dependencies
+import { BrowserRouter as Router } from "react-router-dom";
+>>>>>>> 4bd3f83ad926b0dbc195ab09bf43e40ac47b64d5
 
 function App() {
   return (
